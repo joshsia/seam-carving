46121e8137d9a66690d3960312d8fabdfa4fcfec
joshsia <joshuasia2000@gmail.com>
2021-12-27 16:15:54 -0800

    Add script to compute energy function of image

--- src/get_image.py
+++ src/get_image.py
@@ -9,7 +9,7 @@ get_image.py --url=<url> [--out_file=<out_file>]
 
 Options:
 --url=<url>             URL to download text from
---out_file=<out_file>   Filename to save corpus as [default: input-image.png]
+--out_file=<out_file>   Filename to save corpus as [default: input-image.jpg]
 '''
 
 # python src/get_image.py --url=https://www.vangoghgallery.com/img/starry_night_full.jpg
