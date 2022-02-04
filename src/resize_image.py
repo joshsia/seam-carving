b1df1eef1a3f9b252a7881df99e70dac12437c3b
joshsia <joshuasia2000@gmail.com>
2022-02-03 20:40:22 -0800

    Resizing not limited in one direction first

--- src/resize_image.py
+++ src/resize_image.py
@@ -18,6 +18,10 @@ Options:
 
 # python src/resize_image.py --in_image=img/input-image.jpg
 
+# Note: Currently not good at expanding image because once a seam is added,
+# it will lower the average energy in the seam's direction so it will
+# expand in one way first and then the other which causes artifacts
+
 import sys
 from docopt import docopt
 import numpy as np
@@ -31,8 +35,9 @@ def main(opt):
     img = plt.imread(opt["--in_image"])
     resized_img = seam_carve(img, float(opt["--height"]), float(opt["--width"]))
 
-    out_file = "img/" + opt["--out_file"]
+    print("\n\nFinished resizing image")
 
+    out_file = "img/" + opt["--out_file"]
     plt.imsave(out_file, np.ascontiguousarray(resized_img))
 
 
@@ -47,8 +52,8 @@ def find_vertical_seam(energy):
 
     Returns
     -------
-    list 
-        the seam of column indices
+    tuple 
+        list of column indices describing seam and minimum energy of seam
 
     Example
     -------
@@ -77,14 +82,17 @@ def find_vertical_seam(energy):
     seam = np.zeros(nrows, dtype=int)
     seam[-1] = np.argmin(CME[-1,:])
 
+    min_energy = 0
+
     # Track the path backwards
     for i in range(nrows-2,-1,-1):
         # min_index is 0, 1, or 2. Subtract 1 to give the offset from
         # seam(i+1), namely -1, 0, or 1. Then add this to the old value.
         delta = np.argmin(CME[i, seam[i+1]-1:seam[i+1]+2]) - 1
         seam[i] = seam[i+1] + delta
+        min_energy += np.min(CME[i, seam[i+1]-1:seam[i+1]+2])
 
-    return seam - 1 
+    return (seam - 1, min_energy / len(seam))
     # -1 because the indices are all off by 1, due to padding of CME
 
 
@@ -99,8 +107,8 @@ def find_horizontal_seam(energy):
 
     Returns
     -------
-    numpy.ndarray
-        a seam represented as a 1d array of length M, with all values between 0 and N-1
+    tuple 
+        list of column indices describing seam and minimum energy of seam
     """
 
     return find_vertical_seam(energy.T)
@@ -229,60 +237,108 @@ def seam_carve(image, desired_height, desired_width):
         the resized image
     """
 
+    if (desired_width <= 0 or desired_height <= 0):
+        raise ValueError("Desired height and width cannot be less than 0")
+
     original_width = image.shape[1]
     original_height = image.shape[0]
 
-    if desired_width <= 1:
-        # Removing vertical seams (i.e. decreasing width)
-        while image.shape[1] >= (desired_width * original_width):
-            seam = find_vertical_seam(get_energy(image))
-            assert len(seam) == image.shape[0], "the length of the seam must equal the height of the image"
-            
-            image = remove_vertical_seam(image, seam)
-            print(f"\rWidth is now {round(100 * image.shape[1] / original_width, 1)}% of original", end="")
+    if (desired_width <= 1 and desired_height <= 1):
+        # Removing vertical and horizontal seams (i.e. decreasing both width and height)
+        while ((image.shape[1] >= (desired_width * original_width)) or
+        (image.shape[0] >= (desired_height * original_height))):
+            print(f"Height: {round(100 * image.shape[0] / original_height, 1)}%, "
+            f"Width: {round(100 * image.shape[1] / original_width, 1)}%", end="\r")
+            if (image.shape[1] < (desired_width * original_width)) and (image.shape[0] >= (desired_height * original_height)):
+                # If reached desired width only but not height, only remove horizontal seams
+                h_seam, h_energy = find_horizontal_seam(get_energy(image))
+                image = remove_horizontal_seam(image, h_seam)
+            elif (image.shape[0] < (desired_height * original_height)) and (image.shape[1] >= (desired_width * original_width)):
+                # If reached desired height only but not width, only remove vertical seams
+                v_seam, v_energy = find_vertical_seam(get_energy(image))
+                image = remove_vertical_seam(image, v_seam)
+            else:
+                # Check whether vertical or horizontal seam has lower energy
+                v_seam, v_energy = find_vertical_seam(get_energy(image))
+                h_seam, h_energy = find_horizontal_seam(get_energy(image))
+                if (v_energy > h_energy):
+                    # Remove horizontal seam
+                    image = remove_horizontal_seam(image, h_seam)
+                else:
+                    image = remove_vertical_seam(image, v_seam)
 
-        if desired_height <= 1:
-            # Removing horizontal seams (i.e. decreasing height)
-            while image.shape[0] >= (desired_height * original_height):
-                seam = find_horizontal_seam(get_energy(image))
-                assert len(seam) == image.shape[1], "the length of the seam must equal the width of the image"
-
-                image = remove_horizontal_seam(image, seam)
-                print(f"\rHeight is now {round(100 * image.shape[0] / original_height, 1)}% of original", end="")
-        else:
-            # Adding horizontal seams (i.e increasing height)
-            while image.shape[0] < (desired_height * original_height):
-                seam = find_horizontal_seam(get_energy(image))
-                assert len(seam) == image.shape[1], "the length of the seam must equal the width of the image"
-
-                image = add_horizontal_seam(image, seam)
-                print(f"\rHeight is now {round(100 * image.shape[0] / original_height, 1)}% of original", end="")
+    elif (desired_width <= 1 and desired_height > 1):
+        # Removing vertical seams but adding horizontal seams (i.e. decreasing width but increasing height)
+        while ((image.shape[1] >= (desired_width * original_width)) or
+        (image.shape[0] < (desired_height * original_height))):
+            print(f"Height: {round(100 * image.shape[0] / original_height, 1)}%, "
+            f"Width: {round(100 * image.shape[1] / original_width, 1)}%", end="\r")
+            if (image.shape[1] < (desired_width * original_width)) and (image.shape[0] < (desired_height * original_height)):
+                # If reached desired width but not height, add horizontal seams
+                h_seam, h_energy = find_horizontal_seam(get_energy(image))
+                image = add_horizontal_seam(image, h_seam)
+            elif (image.shape[1] >= (desired_width * original_width)) and (image.shape[0] > (desired_height * original_height)):
+                # If reached desired height but not width, remove vertical seams
+                v_seam, v_energy = find_vertical_seam(get_energy(image))
+                image = remove_vertical_seam(image, v_seam)
+            else:
+                v_seam, v_energy = find_vertical_seam(get_energy(image))
+                h_seam, h_energy = find_horizontal_seam(get_energy(image))
+                if (v_energy > h_energy):
+                    # Add horizontal seam
+                    image = add_horizontal_seam(image, h_seam)
+                else:
+                    # Remove vertical seam
+                    image = remove_vertical_seam(image, v_seam)
+    elif (desired_width > 1 and desired_height <= 1):
+        # Removing horizontal seams but adding vertical seams (i.e. increasing width but decreasing height)
+        while ((image.shape[1] < (desired_width * original_width)) or
+        (image.shape[0] >= (desired_height * original_height))):
+            print(f"Height: {round(100 * image.shape[0] / original_height, 1)}%, "
+            f"Width: {round(100 * image.shape[1] / original_width, 1)}%", end="\r")
+            if (image.shape[1] < (desired_width * original_width)) and (image.shape[0] < (desired_height * original_height)):
+                # If reached desired height but not width, add vertical seams
+                v_seam, v_energy = find_vertical_seam(get_energy(image))
+                image = add_vertical_seam(image, v_seam)
+            elif (image.shape[1] >= (desired_width * original_width)) and (image.shape[0] > (desired_height * original_height)):
+                # If reached desired width but not height, remove horizontal seams
+                h_seam, h_energy = find_horizontal_seam(get_energy(image))
+                image = remove_horizontal_seam(image, h_seam)
+            else:
+                v_seam, v_energy = find_vertical_seam(get_energy(image))
+                h_seam, h_energy = find_horizontal_seam(get_energy(image))
+                if (v_energy > h_energy):
+                    # Remove horizontal seam
+                    image = remove_horizontal_seam(image, h_seam)
+                else:
+                    # Add vertical seam
+                    image = add_vertical_seam(image, v_seam)
+    elif (desired_width > 1 and desired_height > 1):
+        # Adding vertical and horizontal seams (i.e. increasing both width and height)
+        while ((image.shape[1] < (desired_width * original_width)) or
+        (image.shape[0] < (desired_height * original_height))):
+            print(f"Height: {round(100 * image.shape[0] / original_height, 1)}%, "
+            f"Width: {round(100 * image.shape[1] / original_width, 1)}%", end="\r")
+            if (image.shape[1] >= (desired_width * original_width)) and (image.shape[0] < (desired_height * original_height)):
+                # If reached desired width only but not height, only add horizontal seams
+                h_seam, h_energy = find_horizontal_seam(get_energy(image))
+                image = add_horizontal_seam(image, h_seam)
+            elif (image.shape[0] >= (desired_height * original_height)) and (image.shape[1] < (desired_width * original_width)):
+                # If reached desired height only but not width, only add vertical seams
+                v_seam, v_energy = find_vertical_seam(get_energy(image))
+                image = add_vertical_seam(image, v_seam)
+            else:
+                # Check whether vertical or horizontal seam has lower energy
+                v_seam, v_energy = find_vertical_seam(get_energy(image))
+                h_seam, h_energy = find_horizontal_seam(get_energy(image))
+                if (v_energy > h_energy):
+                    # Add horizontal seam
+                    image = add_horizontal_seam(image, h_seam)
+                else:
+                    image = add_vertical_seam(image, v_seam)
     else:
-        # Adding vertical seams (i.e. increasing width)
-        while image.shape[1] < (desired_width * original_width):
-            seam = find_vertical_seam(get_energy(image))
-            assert len(seam) == image.shape[0], "the length of the seam must equal the height of the image"
-            
-            image = add_vertical_seam(image, seam)
-            print(f"\rWidth is now {round(100 * image.shape[1] / original_width, 1)}% of original", end="")
-
-        if desired_height <= 1:
-            # Removing horizontal seams (i.e. decreasing height)
-            while image.shape[0] >= (desired_height * original_height):
-                seam = find_horizontal_seam(get_energy(image))
-                assert len(seam) == image.shape[1], "the length of the seam must equal the width of the image"
-
-                image = remove_horizontal_seam(image, seam)
-                print(f"\rHeight is now {round(100 * image.shape[0] / original_height, 1)}% of original", end="")
-        else:
-            # Adding horizontal seams (i.e increasing height)
-            while image.shape[0] < (desired_height * original_height):
-                seam = find_horizontal_seam(get_energy(image))
-                assert len(seam) == image.shape[1], "the length of the seam must equal the width of the image"
-
-                image = add_horizontal_seam(image, seam)
-                print(f"\rHeight is now {round(100 * image.shape[0] / original_height, 1)}% of original", end="")
-    
+        raise ValueError(
+            "Make sure desired height and width are both greater than 1 or between 0 and 1")
     return image
 
 
